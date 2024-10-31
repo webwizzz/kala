fda99b5506b2a8a777efef937e8ce154f1739941
webwizzz <jatin.tomar@somaiya.edu>
2024-10-31 13:59:38 +0530

    footer

--- kala_main/app/components/Footer.jsx
+++ kala_main/app/components/Footer.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-full h-screen @bg-zinc-900 p-20'>
+    <div className='w-full flex gap-5 h-screen @bg-zinc-900 p-20'>
         <div className='w-1/2 font-["Founders Grotesk X-Condensed"] text-black tracking-tighter'>
-            <h1 className='text-[8vw] leading-none tracking-tighter font-semibold -mb-8'>KALA-</h1>
-            <h1 className='text-[8vw] leading-none tracking-tighter font-semibold -mb-8'>KAR</h1>
+            <h1 className='text-[8vw] leading-none tracking-tighter font-semibold -mb-5'>KALA-</h1>
+            <h1 className='text-[8vw] leading-none tracking-tighter font-semibold -mb-5'>KAR</h1>
         </div>
-    <div className='w-1/2'></div>
+        <div className='w-1/2 font-["Founders Grotesk X-Condensed"] text-black tracking-tighter'>
+            <h1 className='text-[6vw] leading-none tracking-tighter uppercase font-semibold -mb-8'>presentations</h1>
+            <div className="dets">
+                    <a href="#">Facebook</a>
+                    <a href="#">Instagram</a>
+                    <a href="#">Twitters</a>
+            </div>
+            
+        </div>
     </div>
   )
 }
