94803071f065a8404b1013157eeec5d4ac04627d
webwizzz <jatin.tomar@somaiya.edu>
2024-10-31 14:51:20 +0530

    locomotive scrolling added

--- kala_main/app/components/Features.jsx
+++ kala_main/app/components/Features.jsx
@@ -3,51 +3,53 @@ import React from 'react'
 const Features = () => {
   return (
     <div className='w-full py-10'>
-        <div className='w-full px-20 border-b-[2px] border-zinc-700 pb-20' >
-            <h1 className='text-zinc-800 text-8xl font-["Neue Montreal tracking-tight"]'>Features</h1>
-           
-        </div>
-        <div className='px-20'>
+      <div className='w-full px-20 border-b-[2px] border-zinc-700 pb-20'>
+        <h1 className='text-zinc-800 text-8xl font-["Neue Montreal tracking-tight"]'>Features</h1>
+      </div>
+      
+      <div className='px-20'>
         <div className="cards w-full flex gap-10 mt-10">
-                <div className='cardcontainer relative  w-1/2 h-[95vh] '>
-                        <h1 className='absolute text-black z-10  left-full -translate-x-1/2 -translate-y-1/2 top-1/2 leading-none tracking-tighter text-8xl'>
-                        {"KalaKart" .split('').map((item, index)=><span>{item}</span>)}
-                        </h1>
-                    <div className=' card w-full h-full rounded-xl bg-green-600 overflow-hidden'>
-                        <img className=" bg-cover " src='/kart.jpg' width={650} />
-                    </div>
-                </div>
-                <div className='cardcontainer relative rounded-xl w-1/2 h-[95vh]  '>
-                        <h1 className='absolute text-black z-10  right-full translate-x-1/2 -translate-y-1/2 top-1/2 leading-none tracking-tighter text-8xl'>
-                        {"KalaKart" .split('').map((item, index)=><span>{item}</span>)}
-                        </h1>
-                    <div className=' card w-full h-full rounded-xl bg-green-600'>
-                        <img className=' h-full bg-cover ' src='learn.jpg '  width={650}/>
-                    </div>
-                </div>
+          <div className='cardcontainer relative w-1/2 h-[95vh]'>
+            <h1 className='absolute text-black z-10 left-full -translate-x-1/2 -translate-y-1/2 top-1/2 leading-none tracking-tighter text-8xl'>
+              {"KalaKart".split('').map((item, index) => <span key={index}>{item}</span>)}
+            </h1>
+            <div className='card w-full h-full rounded-xl bg-green-600 overflow-hidden'>
+              <img className="bg-cover" src='/kart.jpg' width={650} alt="KalaKart" />
             </div>
-            <div className="cards w-full flex gap-10 mt-10">
-                <div className='cardcontainer relative  w-1/2 h-[95vh] '>
-                        <h1 className='absolute text-black z-10  left-full -translate-x-1/2 -translate-y-1/2 top-1/2 leading-none tracking-tighter text-8xl'>
-                        {"KalaKart" .split('').map((item, index)=><span>{item}</span>)}
-                        </h1>
-                    <div className=' card w-full h-full rounded-xl bg-green-600 overflow-hidden'>
-                        <img className=" bg-cover " src='/kareer.jpg' width={650} />
-                    </div>
-                </div>
-                <div className='cardcontainer relative rounded-xl w-1/2 h-[95vh]  '>
-                        <h1 className='absolute text-black z-10  right-full translate-x-1/2 -translate-y-1/2 top-1/2 leading-none tracking-tighter text-8xl'>
-                        {"KalaKart" .split('').map((item, index)=><span>{item}</span>)}
-                        </h1>
-                    <div className=' card w-full h-full rounded-xl bg-green-600'>
-                        <img className=' h-full bg-cover ' src='culture.jpg '  width={650}/>
-                    </div>
-                </div>
+          </div>
+          
+          <div className='cardcontainer relative rounded-xl w-1/2 h-[95vh]'>
+            <h1 className='absolute text-black z-10 right-full translate-x-1/2 -translate-y-1/2 top-1/2 leading-none tracking-tighter text-8xl'>
+              {"KalaKart".split('').map((item, index) => <span key={index}>{item}</span>)}
+            </h1>
+            <div className='card w-full h-full rounded-xl bg-green-600'>
+              <img className='h-full bg-cover' src='learn.jpg' width={650} alt="Learn" />
             </div>
+          </div>
         </div>
-        
+
+        <div className="cards w-full flex gap-10 mt-10">
+          <div className='cardcontainer relative w-1/2 h-[95vh]'>
+            <h1 className='absolute text-black z-10 left-full -translate-x-1/2 -translate-y-1/2 top-1/2 leading-none tracking-tighter text-8xl'>
+              {"KalaKart".split('').map((item, index) => <span key={index}>{item}</span>)}
+            </h1>
+            <div className='card w-full h-full rounded-xl bg-green-600 overflow-hidden'>
+              <img className="bg-cover" src='/kareer.jpg' width={650} alt="Kareer" />
+            </div>
+          </div>
+          
+          <div className='cardcontainer relative rounded-xl w-1/2 h-[95vh]'>
+            <h1 className='absolute text-black z-10 right-full translate-x-1/2 -translate-y-1/2 top-1/2 leading-none tracking-tighter text-8xl'>
+              {"KalaKart".split('').map((item, index) => <span key={index}>{item}</span>)}
+            </h1>
+            <div className='card w-full h-full rounded-xl bg-green-600'>
+              <img className='h-full bg-cover' src='culture.jpg' width={650} alt="Culture" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Features
+export default Features;
--- kala_main/app/page.js
+++ kala_main/app/page.js
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useRef } from 'react'
 import About from './components/About'
 import Eyes from './components/Eyes'
 import Features from './components/Features'
@@ -6,25 +8,35 @@ import Footer from './components/Footer'
 import LandingPage from './components/LandingPage'
 import Marquee from './components/Marquee'
 import Navbar from './components/Navbar'
+import LocomotiveScroll from 'locomotive-scroll';
 
+function Page() {
+  const scrollRef = useRef(null);
 
+  useEffect(() => {
+    if (scrollRef.current) {
+      const locomotiveScroll = new LocomotiveScroll({
+        el: scrollRef.current,
+        smooth: true,
+      });
 
+      return () => {
+        locomotiveScroll.destroy(); // Clean up on component unmount
+      };
+    }
+  }, []);
 
-function page() {
   return (
-    <div>
-      <div className="w-full min-h-screen bg-[#f5e9da]">
-
-        <Navbar></Navbar>
-        <LandingPage></LandingPage>
-        <Marquee></Marquee>
-        <About></About>
-        <Eyes></Eyes>
-        <Features></Features>
-        <Footer></Footer>
-      </div>
+    <div ref={scrollRef} className="w-full min-h-screen bg-[#f5e9da]">
+      <Navbar />
+      <LandingPage />
+      <Marquee />
+      <About />
+      <Eyes />
+      <Features />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
