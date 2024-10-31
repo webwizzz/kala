b690a6d3b92139f90c3249d912a90eef1318b065
webwizzz <jatin.tomar@somaiya.edu>
2024-10-31 14:34:27 +0530

    footer completed

--- kala_main/app/components/Footer.jsx
+++ kala_main/app/components/Footer.jsx
@@ -7,12 +7,26 @@ const Footer = () => {
             <h1 className='text-[8vw] leading-none tracking-tighter font-semibold -mb-5'>KALA-</h1>
             <h1 className='text-[8vw] leading-none tracking-tighter font-semibold -mb-5'>KAR</h1>
         </div>
-        <div className='w-1/2 font-["Founders Grotesk X-Condensed"] text-black tracking-tighter'>
-            <h1 className='text-[6vw] leading-none tracking-tighter uppercase font-semibold -mb-8'>presentations</h1>
-            <div className="dets">
-                    <a href="#">Facebook</a>
-                    <a href="#">Instagram</a>
-                    <a href="#">Twitters</a>
+        <div className='w-1/2 font-["Founders Grotesk X-Condensed"] text-black tracking-tighter '>
+            <h1 className='text-[6vw] leading-none tracking-tighter uppercase font-semibold mb-8 relative'>presentations</h1>
+            <div className="dets underline pt-10 font-[]">
+                    <a className="block text-xl font-light" href="#">Facebook</a>
+                    <a className="block text-xl font-light" href="#">Instagram</a>
+                    <a className="block text-xl font-light" href="#">Twitters</a>
+                    <a className="block text-xl font-light" href="#">Linkdin</a>
+            </div>
+
+            <div className="dets  underline text-xl gap-5 flex items-center pt-20 ">
+              <p>E:</p>
+                    <a className="block text-xl font-light" href="#">kala@gmail.com</a>
+                   
+            </div>
+
+            <div className="dets absolute pt-20 font-[] right-20">
+            <p className='block text-xl font-light pb-3'>Services :</p>
+                    <a className="block underline text-xl font-light" href="#">KalaKart</a>
+                    <a className="block underline text-xl font-light" href="#">KalaKareer</a>
+                    <a className="block underline text-xl font-light" href="#">KalaLearn</a>
             </div>
             
         </div>
